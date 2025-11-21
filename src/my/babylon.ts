8a9d2c7fd2733aa1ef33a4dfb7dfd255160bfa8c
QwiglyDee <qwiglydee@gmail.com>
2025-11-22 03:49:09 +0700

    bounds ctx

--- src/my/babylon.ts
+++ src/my/babylon.ts
@@ -5,16 +5,19 @@ import { PickingInfo } from "@babylonjs/core/Collisions/pickingInfo";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import type { EngineOptions } from "@babylonjs/core/Engines/thinEngine";
 import { ILoadingScreen } from "@babylonjs/core/Loading/loadingScreen";
-import { Color4 } from "@babylonjs/core/Maths";
+import { Color4, Vector3 } from "@babylonjs/core/Maths";
 import { Scene } from "@babylonjs/core/scene";
 import { Nullable } from "@babylonjs/core/types";
 import { provide } from "@lit/context";
 import { dbgChanges, debug } from "@utils/debug";
 import { queueEvent } from "@utils/events";
 
-import { IBabylonElem, pickCtx, sceneCtx, sizeCtx } from "./context";
+import { boundsCtx, BoundsInfo, IBabylonElem, pickCtx, sceneCtx, sizeCtx } from "./context";
 import { MoveingCtrl } from "./controllers/appMoving";
 import { PickingCtrl } from "./controllers/appPicking";
+import { BoundingInfo } from "@babylonjs/core/Culling/boundingInfo";
+import { AbstractMesh } from "@babylonjs/core/Meshes/abstractMesh";
+import { Tags } from "@babylonjs/core/Misc/tags";
 
 const ENGOPTIONS: EngineOptions = {
     antialias: true,
@@ -38,6 +41,14 @@ export class MyBabylonElem extends ReactiveElement implements IBabylonElem {
     @provide({ context: sizeCtx })
     @property({ type: Number })
     worldSize = 100;
+
+    @provide({ context: boundsCtx })
+    bounds: Nullable<BoundsInfo> = null;
+
+    @state()
+    _bounds_dirty = false;
+
+    static dumbounds = new BoundingInfo(new Vector3(-1, -1, -1), new Vector3(+1, +1, +1));
 
     @property({ type: Boolean })
     rightHanded = false;
@@ -119,6 +130,9 @@ export class MyBabylonElem extends ReactiveElement implements IBabylonElem {
         queueMicrotask(() => {
             this.scene.executeWhenReady(this.#onready, true);
         });
+
+        this.scene.onNewMeshAddedObservable.add(this.#onchanged);
+        this.scene.onMeshRemovedObservable.add(this.#onchanged);
     }
 
     override disconnectedCallback(): void {
@@ -143,6 +157,11 @@ export class MyBabylonElem extends ReactiveElement implements IBabylonElem {
         this.scene = new Scene(this.engine);
         this.scene.useRightHandedSystem = this.rightHanded;
         this.scene.clearColor = Color4.FromHexString(this.background);
+
+        this.bounds = {
+            model: MyBabylonElem.dumbounds,
+            world: MyBabylonElem.dumbounds,
+        };
     }
 
     _dispose() {
@@ -154,6 +173,7 @@ export class MyBabylonElem extends ReactiveElement implements IBabylonElem {
         // debug(this, "ready");
         this.#startRendering();
         this.engine.hideLoadingUI();
+        this._bounds_dirty = true;
         queueEvent(this, "babylon.init");
     };
 
@@ -178,10 +198,21 @@ export class MyBabylonElem extends ReactiveElement implements IBabylonElem {
         }
     };
 
-    // override update(changes: PropertyValues): void {
-    //     debug(this, "updating", dbgChanges(this, changes));
-    //     super.update(changes);
-    // }
+    #stuffilter = (m: AbstractMesh) => Tags.MatchesQuery(m, "!aux");
+
+    #onchanged = (mesh: AbstractMesh) => {
+        this._bounds_dirty = this.#stuffilter(mesh);
+        // TODO: something else
+    };
+
+    override update(changes: PropertyValues): void {
+        debug(this, "updating", dbgChanges(this, changes));
+        if (changes.has("_bounds_dirty") && this._bounds_dirty) {
+            this._bounds_dirty = false;
+            this.bounds = this.getBounds();
+        }
+        super.update(changes);
+    }
 
     override updated(changes: PropertyValues): void {
         if (changes.has("picked")) {
@@ -197,5 +228,29 @@ export class MyBabylonElem extends ReactiveElement implements IBabylonElem {
                 queueEvent(this, "babylon.pick", null);
             }
         }
+    }
+
+    getBounds(): Nullable<BoundsInfo> {
+        const ext = this.scene.getWorldExtends(this.#stuffilter);
+        if (ext.min.y === Number.MAX_VALUE) return null;
+
+        // mirrored bbox
+        const flp = { min: ext.min.scale(-1), max: ext.max.scale(-1) };
+
+        return {
+            model: new BoundingInfo(ext.min, ext.max),
+            world: new BoundingInfo(
+                new Vector3(
+                    Math.min(ext.min.x, flp.min.x, ext.max.x, flp.max.x),
+                    Math.min(ext.min.y, flp.min.y, ext.max.y, flp.max.y),
+                    Math.min(ext.min.z, flp.min.z, ext.max.z, flp.max.z)
+                ),
+                new Vector3(
+                    Math.max(ext.min.x, flp.min.x, ext.max.x, flp.max.x),
+                    Math.max(ext.min.y, flp.min.y, ext.max.y, flp.max.y),
+                    Math.max(ext.min.z, flp.min.z, ext.max.z, flp.max.z)
+                )
+            ),
+        };
     }
 }
--- src/my/context.ts
+++ src/my/context.ts
@@ -4,6 +4,7 @@ import { createContext } from "@lit/context";
 import { Scene } from "@babylonjs/core/scene";
 import { PickingInfo } from "@babylonjs/core/Collisions/pickingInfo";
 import { Nullable } from "@babylonjs/core/types";
+import { BoundingInfo } from "@babylonjs/core/Culling/boundingInfo";
 
 export interface IBabylonElem extends ReactiveElement {
     /** some internal API **/
@@ -12,7 +13,13 @@ export interface IBabylonElem extends ReactiveElement {
     picked: Nullable<PickingInfo>;
 }
 
+export interface BoundsInfo {
+    model: BoundingInfo; // all (non-aux) objects in scene 
+    world: BoundingInfo; // the bounds symmetric around 0
+}
+
 export const sizeCtx = createContext<number>(Symbol('babylon.size'))
 export const sceneCtx = createContext<Scene>(Symbol('babylon.scene'));
 export const pickCtx = createContext<Nullable<PickingInfo>>(Symbol('babylon.pick'));
+export const boundsCtx = createContext<Nullable<BoundsInfo>>(Symbol('babylon.bounds'));
 
